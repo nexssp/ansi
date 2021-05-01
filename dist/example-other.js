fb21762f82a1242067d56a5aa0575a17986d1a14
Marcin Polak <mapoart@gmail.com>
2021-05-01 23:40:32 +0200

    Changed minimizer + cleanup

---
+++ dist/example-other.js
@@ -0,0 +1,1 @@
+const ansi=require("../");let x=0;console.log(ansi.clearScreen(),ansi.up(),ansi.up(),ansi.left(),ansi.yellowBG(ansi.black(`number of colors: ${ansi.colors.length}`)),ansi.down());const f=setInterval((()=>{x<=ansi.colors.length-1?(console.log(ansi.colors[x]),console.log(ansi.up(),ansi[ansi.colors[x]]("Color: "+x,"   ",ansi.colors[x]," ".repeat(10)))):clearInterval(f),x++}),300);
--- tests/ansi.test.js
+++
@@ -1,6 +0,0 @@
-const ansi = require('../src/ansi');
-
-test('ansi.colors', (done) => {
-  expect(Array.isArray(ansi.colors)).toBe(true);
-  done();
-});
